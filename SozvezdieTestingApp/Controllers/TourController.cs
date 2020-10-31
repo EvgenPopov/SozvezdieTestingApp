f88bb9a72d8bc6c31b2e75e85cf9276fa6003ef0
EvgenPopov <popov_evgeniy94@bk.ru>
2020-10-31 20:49:01 +0300

    added : Getting information from json, starting page with, tour information model, starting page with all tours

---
+++ SozvezdieTestingApp/Controllers/TourController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SozvezdieTestingApp.Models;
+
+namespace SozvezdieTestingApp.Controllers
+{
+    public class TourController : Controller
+    {
+        private readonly IDeserializeJson deserialize;
+
+        public TourController(IDeserializeJson deserialize)
+        {
+            this.deserialize = deserialize;
+        }
+
+
+
+
+        public IActionResult Index()
+        {
+            var Tours = deserialize.GetInformation();
+
+            return View(Tours);
+
+        }
+    }
+}
---
+++ SozvezdieTestingApp/Models/TourInforationRepository.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SozvezdieTestingApp.Models
+{
+    public class TourInforationRepository : IDeserializeJson
+    {
+        public TourInformation[] TourInfo { get; private set; }
+
+
+
+        public TourInformation[] GetInformation()
+        {
+            string json = File.ReadAllText(@"C:\Users\Евгений\Downloads\demo_offers.json");
+
+            var pageInfo = JsonConvert.DeserializeObject<ICollection<TourInformation>>(json).ToArray();
+
+            return pageInfo;
+        }
+    }
+}
